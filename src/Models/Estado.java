9db02773e4bd52678c16d043d6e6295c470a4196
Luís PC_CASA <luishfmendes226@gmail.com>
2026-06-12 22:46:09 -0300

    feat(Estado:model): Cria model da entidade estado(id,nome).

--- src/Models/Estado.java
+++ src/Models/Estado.java
@@ -3,4 +3,9 @@ package Models;
 public class Estado {
     public Long id;
     public String nome;
+
+    public Estado(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
 }
