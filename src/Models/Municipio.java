2c456447ab5b7bd70e2821f6e74a3c5faf3bbdd5
Notebook Isa (:Luís) <luishfmendes226@gmail.com>
2026-06-13 12:44:07 -0300

    feat(Municipio:Model): Adiciona o atributo nome_estado.
    - Ao adicionar-lo, permito que sejá possivel padronizar o Get's e usar-lo no Update, de forma que não precise criar vários construtores para diferentes Updates trazendo um só padrão.

--- src/Models/Municipio.java
+++ src/Models/Municipio.java
@@ -1,13 +1,15 @@
 package Models;
 
 public class Municipio {
-    Long id_municipio;
-    String nome;
-    Long id_estado;
+    public Long id_municipio;
+    public String nome;
+    public Long id_estado;
+    public String nome_estado;
 
-    public Municipio(Long id_municipio, String nome, Long id_estado) {
+    public Municipio(Long id_municipio, String nome, Long id_estado, String nome_estado) {
         this.id_municipio = id_municipio;
         this.nome = nome;
         this.id_estado = id_estado;
+        this.nome_estado = nome_estado;
     }
 }
