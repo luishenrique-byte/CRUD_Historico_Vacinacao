5110b942ab1c1a4b3fba3fadebc06fd5a76e9b01
Notebook Isa (:Luís) <luishfmendes226@gmail.com>
2026-06-14 19:00:26 -0300

    feat(Main): inclui comentários para teste dos services.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
 import Repositories.estado.EstadoRepository;
+import Repositories.fabricante.FabricanteRepository;
+import Repositories.fabricante.IFabricanteRepository;
+import Repositories.municipio.MunicipioRepository;
+import Services.estado.EstadoService;
+import Services.fabricante.FabricanteService;
+import Services.fabricante.IFabricanteService;
+import Services.municipio.MunicipioService;
 import conectaDB.ConectaPostgres;
 
 import java.sql.Connection;
@@ -30,5 +37,29 @@ public class Main {
         String password = "12345";
 
         banco.Conectar(url, user, password);
+
+
+//        ===============================
+//        EstadoRepository repository = new EstadoRepository(banco.getCon());
+//        EstadoService service = new EstadoService(repository);
+//        service.modificarNomeEstado(1, "Bahia");
+
+//        ========================================
+        MunicipioRepository repository = new MunicipioRepository(banco.getCon());
+        MunicipioService service = new MunicipioService(repository);
+//        service.listaTodosMunicipios();
+        service.mostrarMunicipioPorId(100);
+//        service.modicarNomeMunicipio(12,"melhor teste que tem");
+
+//        ===========================
+//        IFabricanteRepository repo = new FabricanteRepository(banco.getCon());
+//        IFabricanteService service = new FabricanteService(repo);
+//        service.adicionarFabricante("TESTE 14/06");
+//        service.listarTodosFabricante();
+//        service.mostrarFabricantePorId(5);
+//        service.modificarNomeFabricante(4,"Melhor TESTE 14/06");
+
+        //==================================
+
     }
 }
