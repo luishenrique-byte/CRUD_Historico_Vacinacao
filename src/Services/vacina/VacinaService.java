b66bd2c0681b77f0b86d39773854aad11be5b1e2
Luís PC_CASA <luishfmendes226@gmail.com>
2026-06-15 18:16:26 -0300

    feat(Vacina:service): Implementa a lógica do método adicionarVacina;

--- src/Services/vacina/VacinaService.java
+++ src/Services/vacina/VacinaService.java
@@ -1,9 +1,24 @@
 package Services.vacina;
 
+import Repositories.vacina.IVacinaRepository;
+
 public class VacinaService implements IVacinaService{
+
+    IVacinaRepository _repository;
+
+    public VacinaService(IVacinaRepository _repository) {
+        this._repository = _repository;
+    }
+
     @Override
     public void adicionarVacina(String nome, long id_fabricante, int intervalo_doses) {
+        Integer linhaCriada = _repository.createVacina(nome, id_fabricante, intervalo_doses);
 
+        if (linhaCriada != null && linhaCriada > 0){
+            System.out.println("Vacina adicionada com sucesso.");
+        } else {
+            System.out.println("Não foi possível criar a Vacina.");
+        }
     }
 
     @Override
