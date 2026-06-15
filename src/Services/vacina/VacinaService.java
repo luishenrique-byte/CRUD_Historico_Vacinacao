4be64c4a4052d1294076dbb7d6a9a2f78f79c838
Luís PC_CASA <luishfmendes226@gmail.com>
2026-06-15 18:52:36 -0300

    feat(vacina:service): implementa métodos do VacinaService para Vacina
    - listaTodosVacinas: lista todas as vacinas com fabricante
    - mostrarVacinaPorId: exibe vacina específica por ID
    - modicarNomeVacina: atualiza o nome de uma vacina existente

--- src/Services/vacina/VacinaService.java
+++ src/Services/vacina/VacinaService.java
@@ -1,6 +1,9 @@
 package Services.vacina;
 
+import Models.Vacina;
 import Repositories.vacina.IVacinaRepository;
+
+import java.util.List;
 
 public class VacinaService implements IVacinaService{
 
@@ -23,16 +26,43 @@ public class VacinaService implements IVacinaService{
 
     @Override
     public void listaTodosVacinas() {
+        List<Vacina> vacinaList = _repository.getAllVacina();
 
+        System.out.println("Id  |  Nome  |  Intervalo_Doses | Fabricante");
+        for (Vacina v : vacinaList){
+            System.out.println("[ "+v.id+" | "+v.nome+" | "+v.intervalo_doses+" | "+v.nome_fabricante+" ]");
+        }
     }
 
     @Override
     public void mostrarVacinaPorId(long id) {
+        Vacina v = _repository.getVacinaById(id);
+
+        System.out.println("Id  |  Nome  |  Intervalo_Doses | Fabricante");
+        if (v != null){
+            System.out.println("[ "+v.id+" | "+v.nome+" | "+v.intervalo_doses+" | "+v.nome_fabricante+" ]");
+        } else {
+            System.out.println("Vacina não encontrada a partir desse ID.");
+        }
 
     }
 
     @Override
-    public void modicarNomeVacina(long id, String nome) {
+    public void modicarNomeVacina(long id, String novoNome) {
+        Vacina v = _repository.getVacinaById(id);
 
+        if (v != null){
+            System.out.println("Vacina não encontrada para esse ID.");
+            return;
+        }
+
+        Integer qtdeLinhas = _repository.updateVacina(new Vacina(v.id, novoNome, v.intervalo_doses, v.id_fabricante, v.nome_fabricante));
+
+        if (qtdeLinhas != null && qtdeLinhas>0) {
+            System.out.println("Operação realizada com sucessor.");
+            System.out.println(qtdeLinhas + " linha afetada.");
+        } else {
+            System.out.println("Nenhuma linha foi afetada. Reveja o ID.");
+        }
     }
 }
