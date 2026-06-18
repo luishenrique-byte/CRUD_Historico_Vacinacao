92faf4faab43130ed5bbf4828cacbe5bf8987046
Luís PC_CASA <luishfmendes226@gmail.com>
2026-06-17 21:19:42 -0300

    feat(model): adiciona Models de retorno para as functions de registro_vacinacao usada no Repository
    - QtdeDoses: retorno de func_qtde_doses (nome_paciente, qtde_doses)
    - QtdeDosesXVacina: retorno de func_qtde_doses_x_vacina (vacina, qtde_doses)
    - ProximaDose: retorno de func_prox_dose (nome_paciente, vacina,
      data_vacinacao_atual, data_prox_dose)
    
    Models específicos criados em vez de reaproveitar Paciente/Vacina, já
    que as functions retornam combinações de campos calculados que não
    correspondem a nenhuma entidade completa do banco.

---
+++ src/Models/for_functions/QtdeDosesXVacina.java
@@ -0,0 +1,11 @@
+package Models.for_functions;
+
+public class QtdeDosesXVacina {
+    public String vacina;
+    public long qtde_doses;
+
+    public QtdeDosesXVacina(String vacina, long qtde_doses) {
+        this.vacina = vacina;
+        this.qtde_doses = qtde_doses;
+    }
+}
