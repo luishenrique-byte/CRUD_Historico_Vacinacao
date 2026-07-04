e6373d8800d446073192cbd88986cc2a682c576a
Luís PC_CASA <luishfmendes226@gmail.com>
2026-07-04 12:57:45 -0300

    chore: adiciona estrutura inicial de diretórios

--- src/main/java/com/luishenrique/cap/Historico_Vacinacao/App.java
+++ src/main/java/com/luishenrique/cap/Historico_Vacinacao/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HistoricoVacinacaoApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HistoricoVacinacaoApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
