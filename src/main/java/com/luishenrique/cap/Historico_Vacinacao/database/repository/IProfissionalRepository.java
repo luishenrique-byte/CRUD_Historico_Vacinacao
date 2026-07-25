bd68701850c2bdd5537ee129b2327dd9ca978249
Luís PC_CASA <luishfmendes226@gmail.com>
2026-07-25 11:39:48 -0300

    feat(profissional:repository): adiciona método de busca pelo documento do profissional usando Query Methods.
    - findByDocumento() retorna uma lista de prestadores a partir do número de documento informado, que corresponde a CPF para CLT's ou CNPJ para PJ.

--- src/main/java/com/luishenrique/cap/Historico_Vacinacao/database/repository/IProfissionalRepository.java
+++ src/main/java/com/luishenrique/cap/Historico_Vacinacao/database/repository/IProfissionalRepository.java
@@ -4,6 +4,9 @@ import com.luishenrique.cap.Historico_Vacinacao.database.models.ProfissionalEnti
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IProfissionalRepository extends JpaRepository<ProfissionalEntity, Long> {
+    List<ProfissionalEntity> findByDocumento(String documento);
 }
