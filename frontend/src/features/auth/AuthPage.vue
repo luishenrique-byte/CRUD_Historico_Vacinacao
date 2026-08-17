e3ebeb6f977299d84fc949be9b1a89724c2692df
Luís PC_CASA <luishfmendes226@gmail.com>
2026-08-17 17:54:55 -0300

    feat(front/AuthPage): adiciona escopo da página de autenticação.
    - É necessário fazer as rotas para que traga da home se é paciente ou profissional.
    - Fazer o input retonar o valor.

---
+++ frontend/src/features/auth/AuthPage.vue
@@ -0,0 +1,71 @@
+<!-- SCRIPT -->
+<script setup>
+import InputText from '@/shared/components/InputText.vue'
+
+//é necessário receber do login se é paciente ou profissional
+//para os atributos
+//src:"" das img
+//interpolação do label
+//interpolação do button
+</script>
+<!-- HTML -->
+<template>
+  <span class="card">
+    <img class="img" draggable="false" src="../../assets/paciente_256x256_pc.png" />
+
+    <span class="label-dinamica"
+      >Informe o CPF do
+      <p>
+        Paciente
+        <!--é necessário fazer e interpolação aqui-->
+      </p></span
+    >
+
+    <InputText label="CPF"></InputText>
+
+    <button class="btn-acess">Acessar Cartão de Vacina<!--Ou WorkFlow--></button>
+  </span>
+</template>
+<!-- Informe o  CPF do PacienteOu profissional -->
+
+<!-- STYLE -->
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  gap: 6%;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+  height: 300px;
+  padding: 20px;
+  border-radius: 15px;
+  background: #fff;
+  box-shadow: 6px 4px 9px 0 rgba(0, 0, 0, 0.25);
+}
+
+.img {
+  height: 30%;
+  width: auto;
+  margin-bottom: 20px;
+}
+
+.label-dinamica {
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+}
+
+.btn-acess {
+  padding: 10px 20px;
+  background: #5d8dd4;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  transition: 0.3s ease;
+}
+.btn-acess:hover {
+  transform: translateY(-5px);
+  cursor: pointer;
+}
+</style>
